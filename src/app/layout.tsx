c951ccf184079ee15c40c80ed01df3564b84d90a
Joehann <jpouponnot@outlook.fr>
2024-11-05 23:57:22 +0100

    fix: compilation settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,9 @@
-"use client"
 import { ReactNode } from "react"
-import { QueryClientProvider } from "@tanstack/react-query"
-import queryClient from "@/core/providers/queryClient"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        <QueryClientProvider client={queryClient}>
-          {children}
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
